5764faa337f2b10804c62b264c2fd2e10a158c7a
wellslu <34208509+wellslu@users.noreply.github.com>
2019-11-11 16:22:24 +0800

    Create var&const.js

---
+++ var&const.js
@@ -0,0 +1,18 @@
+console.log('Hello')
+window.alert('Hello')
+alert('Hello')
+document.write('Hello')
+document.getElementById("demo").innerHTML = 'Hello'
+const newText = document.createTextNode('Hello')
+const demo = document.getElementById("demo")
+demo.appendChild(newText)
+
+const a = 10
+let b = 5
+b = 6
+
+
+const items = getItems()
+const goSportsTeam = true
+const dragonball = 'z'
+console.log(dragonball)
