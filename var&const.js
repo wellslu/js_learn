c9b9b9f17a1177c12958640ed2144de5d2853a7f
wellslu <34208509+wellslu@users.noreply.github.com>
2019-11-11 16:25:50 +0800

    Update var&const.js

--- var&const.js
+++ var&const.js
@@ -1,17 +1,18 @@
-console.log('Hello')
-window.alert('Hello')
-alert('Hello')
-document.write('Hello')
-document.getElementById("demo").innerHTML = 'Hello'
+console.log('Hello')//顯示Hello
+window.alert('Hello')//用警告顯示
+alert('Hello')//等同上面
+document.write('Hello')//在html寫下hello
+document.getElementById("demo").innerHTML = 'Hello'//在html裡id為demo的地方寫hello
+//同上一行效果，但是是以節點去做的
 const newText = document.createTextNode('Hello')
 const demo = document.getElementById("demo")
 demo.appendChild(newText)
 
-const a = 10
-let b = 5
-b = 6
+const a = 10//常值
+let b = 5//變數
+b = 6//變數是可以改變的
 
-
+//常數盡量分開命名
 const items = getItems()
 const goSportsTeam = true
 const dragonball = 'z'
